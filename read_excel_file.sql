cffacc27e34e49346c2a8ab5959bd05c2013823f
Cycrimeber <cycrimeber@gmail.com>
2023-10-09 14:18:18 +0100

    Initial commit of files
    How to read an excel file with php and save/retrieve into/from a database.

---
+++ read_excel_file.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE `excel_readData_db`;
+CREATE TABLE `members` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+    `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+    `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+    `phone` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `created` datetime NOT NULL,
+    `modified` datetime NOT NULL,
+    `status` enum('Active', 'Inactive') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Active',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
---
+++ readme.txt
@@ -0,0 +1,5 @@
+We will use the PhpSpreadsheet library to read Excel files and parse data. The PhpSpreadsheet is a PHP library that helps to parse data from spreadsheet file formats (.xls, .xlsx, etc).
+
+Use composer to install PhpSpreadsheet in the script folder.
+
+composer require phpoffice/phpspreadsheet
